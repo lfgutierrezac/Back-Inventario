5234a686408988ccf72b7d3330214fa757081b8a
lfgutierrezac <fernandagac@gmail.com>
2021-12-02 14:02:48 -0600

    reply by mail for registration and login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 const Employee = require('../models/employee')
 const authService = require('../services/auth.service')
 const { validationResult } = require('express-validator')
+const mail = require('../services/mail.service')
 
 
 const authController = {
@@ -12,6 +13,11 @@ const authController = {
                 res.status(400).json('Email and password required')
             }
             const token = await authService.login(req.body)
+            const data = await mail.send(
+                req.body.email,'Nuevo inicio de sesión en Inventario',
+                'Has iniciado sesión en tu cuenta de Inventario',
+                '<b>¡Hola de nuevo! </b><br> Has iniciado sesión en tu cuenta de Inventario<br /><br>------</br>')
+            // res.send(data)
             res.status(token.code).json(token)
         } catch (error) {
             // res.send(error)
@@ -34,6 +40,11 @@ const authController = {
         try {
             const user = new User(req.body)
             const token = await authService.register(user)
+            const data = await mail.send(
+                req.body.email,'Has creado una cuenta en Inventario',
+                'Tu nueva cuenta ya está activa',
+                '<b>¡Bienvenido! </b><br> Has creado una nueva cuenta en Inventario<br /><br>------</br>')
+            // res.send(data)
             res.status(token.code).json({"token":token})
         } catch (error) {
             res.send(error)
---
+++ src/services/mail.service.js
@@ -0,0 +1,28 @@
+const nodemaider = require('nodemailer')
+
+const mail = {
+    send: async (toMail, subjectMail, textMail, htmlMail) => {
+        const transporter = nodemaider.createTransport({
+            host: process.env.HOST_MAIL,
+            port: process.env.PORT_MAIL,
+            auth: {
+                user: process.env.USER_MAIL,
+                pass: process.env.PASS_MAIL
+            }
+        })
+
+        const mailOptions = {
+            from: process.env.USER_MAIL,
+            to:toMail,
+            subject: subjectMail,
+            text: textMail,
+            html: htmlMail
+        }
+
+        let info = await transporter.sendMail(mailOptions)
+
+        return info
+    }
+}
+
+module.exports = mail
