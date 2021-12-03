7c5f0e765e7794ad4ba522faa9c1032629303b59
lfgutierrezac <fernandagac@gmail.com>
2021-12-02 20:28:44 -0600

    email

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 const Employee = require('../models/employee')
 const authService = require('../services/auth.service')
 const { validationResult } = require('express-validator')
-// const mail = require('../services/mail.service')
+const mail = require('../services/mail.service')
 
 
 const authController = {
@@ -13,12 +13,12 @@ const authController = {
                 res.status(400).json('Email and password required')
             }
             const token = await authService.login(req.body)
-            // const dataEmail = await mail.send(process.env.USER_MAIL,
-            //     req.body.email,'Nuevo inicio de sesión en Inventario',
-            //     'Has iniciado sesión en tu cuenta de Inventario',
-            //     '<b>¡Hola de nuevo! </b><br> Has iniciado sesión en tu cuenta de Inventario<br /><br>------</br>')
-            // res.send(dataEmail)
-            res.status(token.code).json({"token":token})
+            const dataEmail = await mail.send(
+                req.body.email,'Nuevo inicio de sesión en Inventario',
+                'Has iniciado sesión en tu cuenta de Inventario',
+                '<b>¡Hola de nuevo! </b><br> Has iniciado sesión en tu cuenta de Inventario<br /><br>------</br>')
+            res.send(dataEmail)
+            // res.status(token.code).json({"token":token})
         } catch (error) {
             // res.send(error)
             res.status(500).json({"error":error})
@@ -40,10 +40,10 @@ const authController = {
         try {
             const user = new User(req.body)
             const token = await authService.register(user)
-            // const dataEmail = await mail.send(
-            //     req.body.email,'Has creado una cuenta en Inventario',
-            //     'Tu nueva cuenta ya está activa',
-            //     '<b>¡Bienvenido! </b><br> Has creado una nueva cuenta en Inventario<br /><br>------</br>')
+            const dataEmail = await mail.send(
+                req.body.email,'Has creado una cuenta en Inventario',
+                'Tu nueva cuenta ya está activa',
+                '<b>¡Bienvenido! </b><br> Has creado una nueva cuenta en Inventario<br /><br>------</br>')
             // res.send(dataEmail)
             res.status(token.code).json({"token":token})
         } catch (error) {
